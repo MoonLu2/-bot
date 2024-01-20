cd79430340e7d0ee0ab5bc1af0f1b140cc634afa
MoonyLu2 <153979873+MoonLu2@users.noreply.github.com>
2024-01-20 09:48:21 +0200

    Create random.py

---
+++ random.py
@@ -0,0 +1,6 @@
+import random
+
+def symbol_gen():
+    symbol_list = '1234👩‍😎😍🤗🥰😅🤣😂🤐😙😚👋🤛😎💘🥳💟☹😪😱😋%$&amp;()pojuGUIPOJU567890💞🐾😘⏳💌☝🏽✅❌🕑💬👼💎🎹📕🎲LUDA'
+    smiles = random.choice(symbol_list)
+    return smiles
