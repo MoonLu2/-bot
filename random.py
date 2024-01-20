76ccedf68dd188272cfe29266fba71e8be60bdb3
MoonyLu2 <153979873+MoonLu2@users.noreply.github.com>
2024-01-20 20:14:52 +0200

    Update random.py

--- random.py
+++ random.py
@@ -1,6 +1,13 @@
 import random
 
 def symbol_gen():
-    symbol_list = '1234👩‍😎😍🤗🥰😅🤣😂🤐😙😚👋🤛😎💘🥳💟☹😪😱😋%$&amp;()pojuGUIPOJU567890💞🐾😘⏳💌☝🏽✅❌🕑💬👼💎🎹📕🎲LUDA'
-    smiles = random.choice(symbol_list)
-    return smiles
+    symbol_list = '1234%$&amp;()pojuGUIPOJU567890LUDA'
+    book_list = '📕📗📘📙📖📚📓📒'
+    smile_list = '🤗😎😍🤣😂😭😢🤐😐😑😶🙂☺🤩🥰🤔😴🥱😡🥳😇🤠🤥🤫🧐🤭☠'
+    womanandman_list = '👩👨🧑👧👼👩‍💻👨‍💻🧛‍♀️🧛‍♂️🧜‍🧜‍🧚‍🧚‍👸🤴'
+    fav_list = '💍💎🎈🎒👑🛶🎯🏆🧩♟🏹⚔🛡🎁🎀🎄🎃🔮🎲🎹🥁🔒🔓🖌💸⌛'
+
+    main_list = fav_list + womanandman_list + symbol_list + smile_list + book_list
+    symbols = random.choice(main_list)
+
+    return symbols
